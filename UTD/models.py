902dfba146df77559122ff585c76ac44f9c7f1b9
Josep <jap8@alumnes.udl.cat>
2016-04-05 18:05:49 +0200

    Created simple models for Album, artist and song. Migrated to the DB.

--- UTD/models.py
+++ UTD/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Artist(models.Model):
+    name = models.TextField()
+
+
+class Album(models.Model):
+    artist = models.ForeignKey(Artist)
+    release_date = models.DateField()
+
+
+class Song(models.Model):
+    name = models.TextField()
+    album = models.ForeignKey(Album)
