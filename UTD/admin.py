6b2680d3cd8a4280cb9285b4492313b73f924381
Josep <jap8@alumnes.udl.cat>
2016-04-07 10:56:57 +0200

    Added the models to the admin interface, and created a superuser.

--- UTD/admin.py
+++ UTD/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+import models
 
 # Register your models here.
+
+admin.site.register(models.Album)
+admin.site.register(models.Artist)
+admin.site.register(models.Song)
