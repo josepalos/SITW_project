f25c68e1d67e751212b89e383af586ac3c539dd3
Bianca Padurean <biancapadurean@aules-108-072.udl.net>
2016-04-07 17:23:11 +0200

    Started implementing UTD.views

--- UTD/urls.py
+++ UTD/urls.py
@@ -1,2 +1,8 @@
+from django.conf.urls import url
+from UTD.views import list_artists
+
+
+
 urlpatterns = [
+    url(r'^artists/$', list_artists)
 ]
--- UTD/views.py
+++ UTD/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from models import Artist
+from models import Album
+from django.http import HttpResponse
+import json
 
-# Create your views here.
+def list_albums(request, artist_id):
+    pass
+
+def list_artists(request):
+    artists = Artist.objects.all()
+    artistsjson = []
+    for a in artists:
+        artist = dict()
+        artist['name'] = a.name
+        artistsjson.append(artist)
+    return HttpResponse(json.dumps(artistsjson))
+
+def list_songs(request, album_id):
+    pass
+
