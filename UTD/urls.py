f3a46f3fd647c8b40ba9d8f776a54c43d0fe66e3
Josep <jap8@alumnes.udl.cat>
2016-04-07 17:59:06 +0200

    Added the view, the template and the url for the albums list for each artist.

--- UTD/urls.py
+++ UTD/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from UTD.views import list_artists
+from UTD.views import list_artists, list_albums
 
 
 
 urlpatterns = [
-    url(r'^artists/$', list_artists)
+    url(r'^artists/$', list_artists),
+    url(r'^artists/(\d)/albums/$', list_albums),
 ]
--- UTD/views.py
+++ UTD/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
-from models import Artist
-from models import Album
+from models import Artist, Album
 from django.http import HttpResponse
+import datetime
 import json
 
+
 def list_albums(request, artist_id):
-    pass
+    artist = Artist.objects.get(id=artist_id)
+    albums = Album.objects.filter(artist_id=artist_id)
+    return render(
+        request,
+        'albumslist.html',
+        {
+            'titlehead': 'Albums list',
+            'pagetitle': 'Albums from: %s' % artist.name,
+            'albums_list': albums
+        }
+    )
+
 
 def list_artists(request):
     artists = Artist.objects.all()
@@ -16,7 +28,9 @@ def list_artists(request):
             'titlehead': 'Artists list',
             'pagetitle': 'Artists list',
             'artists_list': artists
-        })
+        }
+    )
+
 
 def list_songs(request, album_id):
     pass
