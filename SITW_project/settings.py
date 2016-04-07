3d3482c3cb3299a8c4555e6fe64e8b4bb1c1f64e
Josep <jap8@alumnes.udl.cat>
2016-04-07 16:57:36 +0200

    Moved the templates folder inside the app folder.

--- SITW_project/settings.py
+++ SITW_project/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'SITW_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'UTD/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
