adde71141a4ea495688622da305004e9fcc382e4
Josep <jap8@alumnes.udl.cat>
2016-04-08 18:40:07 +0200

    Corrected the url for artists (added the match to end of the regexp.

--- UTD/urls.py
+++ UTD/urls.py
@@ -6,5 +6,5 @@ from UTD.views import list_artists, list_albums, artist_details
 urlpatterns = [
     url(r'^artists/$', list_artists),
     url(r'^artists/(\d)/albums/$', list_albums),
-    url(r'^artists/(\d)/', artist_details),
+    url(r'^artists/(\d)$/', artist_details),
 ]
