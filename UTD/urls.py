4f85a49641d9c2aa83ab965fa21d646557ce3c27
Josep <jap8@alumnes.udl.cat>
2016-04-10 16:02:20 +0200

    Urls didn't accept ids greater than 9. Corrected.

--- UTD/urls.py
+++ UTD/urls.py
@@ -5,6 +5,6 @@ from UTD.views import ArtistList, ArtistDetails, AlbumList, AlbumDetails
 urlpatterns = [
     url(r'^artists/$', ArtistList.as_view()),
     url(r'^artists/(?P<pk>\d+)/$', ArtistDetails.as_view()),
-    url(r'^artists/(?P<artist_id>\d)/albums/$', AlbumList.as_view()),
-    url(r'^artists/(?P<artist_id>\d)/albums/(?P<pk>\d)/$', AlbumDetails.as_view()),
+    url(r'^artists/(?P<artist_id>\d+)/albums/$', AlbumList.as_view()),
+    url(r'^artists/(?P<artist_id>\d+)/albums/(?P<pk>\d+)/$', AlbumDetails.as_view()),
 ]
