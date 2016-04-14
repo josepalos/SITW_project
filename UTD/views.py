2d230f6b8da29c24ca2f10f4321084860900a234
Josep <jap8@alumnes.udl.cat>
2016-04-14 16:45:54 +0200

    Added class views for songs and songs details.

--- UTD/views.py
+++ UTD/views.py
@@ -1,4 +1,4 @@
-from models import Artist, Album
+from models import Artist, Album, Song
 from django.views.generic import ListView, DetailView
 from django.shortcuts import get_object_or_404
 
@@ -52,8 +52,28 @@ class AlbumDetails(DetailView):
         context['pagetitle'] = 'Album %s' % self.object.name
         return context
 
+
 class SongList(ListView):
-    pass
+    template_name = 'songslist.html'
+    context_object_name = 'songs_list'
+
+    def get_queryset(self):
+        self.album = get_object_or_404(Album, pk=self.kwargs['pk'])
+        return Song.objects.filter(album=self.album)
+
+    def get_context_data(self, **kwargs):
+        context = super(SongList, self).get_context_data(**kwargs)
+        context['titlehead'] = 'Songs list'
+        context['pagetitle'] = 'Songs list'
+        return context
+
 
 class SongDetails(DetailView):
-    pass
+    model = Song
+    template_name = 'songslist.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(SongDetails, self).get_context_data(**kwargs)
+        context['titlehead'] = 'Song %s' % self.object.name
+        context['pagetitle'] = 'Song %s' % self.object.name
+        return context
