947c0348d20533dfa70bef5e67920c568b094649
Josep <jap8@alumnes.udl.cat>
2016-04-14 16:27:26 +0200

    Corrected urls.

--- UTD/urls.py
+++ UTD/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import url
-from UTD.views import list_artists, list_albums, artist_details, list_songs, song_details
+from UTD.views import list_artists, list_albums, artist_details, list_songs, song_details, album_details
 
 
 urlpatterns = [
     url(r'^artists/$', list_artists),
-    url(r'^artists/(\d)/albums/$', list_albums),
-    url(r'^artists/(\d)/$', artist_details),
-    url(r'^album/(\d)/$', list_songs),
-    url(r'^song/(\d)/$', song_details),
+    url(r'^artists/(\d+)/', artist_details),
+    url(r'^artists/(\d+)/albums/$', list_albums),
+
+    url(r'^albums/(\d+)/$', album_details),
+    url(r'^albums/(\d+)/songs/$', list_songs),
+    # url(r'^songs/', list_songs),
+    url(r'^songs/(\d+)/$', song_details),
 ]
