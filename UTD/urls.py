a991769148038c1ac1d6640a269ec4dc2183f6f5
Bianca Padurean <biancapadurean95@gmail.com>
2016-05-09 12:32:20 +0200

    added url for playlists

--- UTD/urls.py
+++ UTD/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url, patterns, include
 from UTD.views import APISongDetail, APIAlbumDetail, APIArtistDetail, APIArtistList, APIUserDetail, APIUserList, \
     APIPlaylistDetail, APIProviderDetail, ProvidersCreate
 from UTD.views import ArtistList, ArtistDetails, AlbumList, AlbumDetails, SongList, SongDetails, RelatedArtistList,\
-    Providers, FollowedArtists, DisplayPlaylist, ProfileView, follow_artist, unfollow_artist, index
+    Providers, FollowedArtists, DisplayPlaylist, ProfileView, follow_artist, unfollow_artist, index, Playlists
 
 
 urlpatterns = [
@@ -25,7 +25,8 @@ urlpatterns = [
     url(r'^user/(?P<username>.+)(?P<format>(\.json|\.xml|\.html)?)/$', ProfileView.as_view(), name='profile'),
     url(r'^user/(?P<username>.+)/following(?P<format>(\.json|\.xml|\.html)?)/$', FollowedArtists.as_view(),
         name='followed_artists'),
-    url(r'^user/(?P<username>.+)/playlist(?P<format>(\.json|\.xml|\.html)?)/$', DisplayPlaylist.as_view()),
+    url(r'^user/(?P<username>.+)/playlists(?P<format>(\.json|\.xml|\.html)?)/$', Playlists.as_view()),
+    #url(r'^user/(?P<username>.+)/playlist(?P<format>(\.json|\.xml|\.html)?)/$', DisplayPlaylist.as_view()),
 ]
 
 
